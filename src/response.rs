0ea88e9dee51eea9c93a3d9867d17bb8762c2f45
Selmeci <selmeci.roman@gmail.com>
2020-03-13 18:32:30 +0100

    Add helper fn to deserialize response into owned T

--- src/response.rs
+++ src/response.rs
@@ -11,4 +11,12 @@ impl Response {
         let result: T = serde_json::from_slice(&self.json)?;
         Ok(result)
     }
+
+    pub fn try_into_owned<T>(&self) -> Result<T, Error>
+    where
+        T: de::DeserializeOwned,
+    {
+        let result: T = serde_json::from_slice(&self.json)?;
+        Ok(result)
+    }
 }
