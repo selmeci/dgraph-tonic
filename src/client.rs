187d33f0011fa78835f3d91e1bdb9220e79107f1
Selmeci <selmeci.roman@gmail.com>
2020-04-19 10:21:46 +0200

    Refactoring: take domain name for client with tls from endpoint uri. Add docs.

--- src/client.rs
+++ src/client.rs
@@ -13,7 +13,11 @@ use crate::{
     BestEffortTxn, DgraphClient, IDgraphClient, MutatedTxn, Operation, Payload, ReadOnlyTxn,
     Result, Txn,
 };
+use http::Uri;
 
+///
+/// Async client for dGraph DB.
+///
 #[derive(Clone)]
 pub struct Client {
     stubs: Vec<Stub>,
@@ -26,8 +30,8 @@ impl Debug for Client {
 }
 
 impl Client {
-    fn balance_list<S: TryInto<Endpoint>>(endpoints: Vec<S>) -> Result<Vec<Endpoint>, Error> {
-        let mut balance_list: Vec<Endpoint> = Vec::new();
+    fn balance_list<S: TryInto<Uri>>(endpoints: Vec<S>) -> Result<Vec<Uri>, Error> {
+        let mut balance_list: Vec<Uri> = Vec::new();
         for maybe_endpoint in endpoints {
             let endpoint = match maybe_endpoint.try_into() {
                 Ok(endpoint) => endpoint,
@@ -53,18 +57,74 @@ impl Client {
         }
     }
 
-    pub async fn new<S: TryInto<Endpoint>>(endpoints: Vec<S>) -> Result<Self, Error> {
+    fn make(stubs: Vec<Stub>) -> Self {
+        Self { stubs }
+    }
+
+    ///
+    /// Create new dGraph client for interacting v DB and try to connect to given endpoints.
+    ///
+    /// The client can be backed by multiple endpoints (to the same server, or multiple servers in a cluster).
+    ///
+    /// # Arguments
+    ///
+    /// * `endpoints` - Vector of possible endpoints
+    ///
+    /// # Errors
+    ///
+    /// * connection to DB fails
+    /// * endpoints vector is empty
+    /// * item in vector cannot by converted into Uri
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use dgraph_tonic::Client;
+    /// let client = Client::new(vec!["http://127.0.0.1:9080"]).await?;
+    /// ```
+    ///
+    pub async fn new<S: TryInto<Uri>>(endpoints: Vec<S>) -> Result<Self, Error> {
         let balance_list = Self::balance_list(endpoints)?;
         let mut stubs = Vec::with_capacity(balance_list.len());
-        for endpoint in balance_list {
+        for uri in balance_list {
+            let endpoint: Endpoint = uri.into();
             let channel = endpoint.connect().await?;
             stubs.push(Stub::new(DgraphClient::new(channel)));
         }
-        Ok(Self { stubs })
+        Ok(Self::make(stubs))
     }
 
-    pub async fn new_with_tls_client_auth<S: TryInto<Endpoint>>(
-        domain_name: impl Into<String>,
+    ///
+    /// Create new dGraph client authorized with SSL cert for interacting v DB and try to connect to given endpoints.
+    ///
+    /// The client can be backed by multiple endpoints (to the same server, or multiple servers in a cluster).
+    ///
+    /// # Arguments
+    ///
+    /// * `endpoints` - Vector of possible endpoints
+    /// * `server_root_ca_cert` - path to server CA certificate
+    /// * `client_cert` - path to client certificate
+    /// * `client_key` - path to client private key
+    ///
+    /// # Errors
+    ///
+    /// * connection to DB fails
+    /// * endpoints vector is empty
+    /// * item in vector cannot by converted into Uri
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use dgraph_tonic::Client;
+    /// let client = Client::new_with_tls_client_auth(
+    ///     vec!["http://127.0.0.1:9080"],
+    ///     "/path/ca.crt",
+    ///     "/path/client.crt",
+    ///     "/path/client.key"
+    /// ).await?;
+    /// ```
+    ///
+    pub async fn new_with_tls_client_auth<S: TryInto<Uri>>(
         endpoints: Vec<S>,
         server_root_ca_cert: impl AsRef<Path>,
         client_cert: impl AsRef<Path>,
@@ -77,16 +137,19 @@ impl Client {
         let client_identity =
             Identity::from_pem(client_cert_future.await?, client_key_future.await?);
         let tls = ClientTlsConfig::new()
-            .domain_name(domain_name)
             .ca_certificate(server_root_ca_cert)
             .identity(client_identity);
-        let balance_list = Self::balance_list(endpoints)?;
+        let balance_list: Vec<Uri> = Self::balance_list(endpoints)?;
         let mut stubs = Vec::with_capacity(balance_list.len());
-        for endpoint in balance_list {
+        for uri in balance_list {
+            let tls = tls
+                .clone()
+                .domain_name(uri.host().expect("host in endpoint"));
+            let endpoint: Endpoint = uri.into();
             let channel = endpoint.tls_config(tls.clone()).connect().await?;
             stubs.push(Stub::new(DgraphClient::new(channel)));
         }
-        Ok(Self { stubs })
+        Ok(Self::make(stubs))
     }
 
     ///
@@ -97,26 +160,59 @@ impl Client {
     }
 
     ///
-    /// Return transaction which can only queries
+    /// Create new transaction which can only do queries.
+    ///
+    /// Read-only transactions are useful to increase read speed because they can circumvent the
+    /// usual consensus protocol.
     ///
     pub fn new_read_only_txn(&self) -> ReadOnlyTxn {
         self.new_txn().read_only()
     }
 
     ///
-    /// Return transaction which can only queries in best effort mode
+    /// Create new transaction which can only do queries in best effort mode.
+    ///
+    /// Read-only queries can optionally be set as best-effort. Using this flag will ask the
+    /// Dgraph Alpha to try to get timestamps from memory on a best-effort basis to reduce the number
+    /// of outbound requests to Zero. This may yield improved latencies in read-bound workloads where
+    /// linearizable reads are not strictly needed.
     ///
     pub fn new_best_effort_txn(&self) -> BestEffortTxn {
         self.new_read_only_txn().best_effort()
     }
 
     ///
-    /// Return transaction which can performs mutate, commit and discard
+    /// Create new transaction which can do mutate, commit and discard operations
     ///
     pub fn new_mutated_txn(&self) -> MutatedTxn {
         self.new_txn().mutated()
     }
 
+    ///
+    /// The /alter endpoint is used to create or change the schema.
+    ///
+    /// # Arguments
+    ///
+    /// - `op`: Alter operation
+    ///
+    /// # Errors
+    ///
+    /// * gRPC error
+    /// * DB reject alter command
+    ///
+    /// # Example
+    ///
+    /// Install a schema into dgraph. A `name` predicate is string type and has exact index.
+    ///
+    /// ```
+    /// use dgraph_tonic::Client;
+    /// let client = Client::new(vec!["http://127.0.0.1:9080"]).await?;
+    /// let op = Operation {
+    ///     schema: "name: string @index(exact) .".into(),
+    ///     ..Default::default()
+    /// };
+    /// ```
+    ///
     pub async fn alter(&self, op: Operation) -> Result<Payload, Status> {
         let mut stub = self.any_client();
         stub.alter(op).await
